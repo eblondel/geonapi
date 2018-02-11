18593f12125878a7f16cfdf55ecb4c9695cb9102
eblondel <emmanuel.blondel1@gmail.com>
2018-02-11 18:08:14 +0100

    try CI tests on pobsteta/docker-geonetwork

--- tests/test-all.R
+++ tests/test-all.R
@@ -5,7 +5,7 @@ library(geonapi)
 gnUrl <- "http://localhost:8080/geonetwork"
 gnUsr <- "admin"
 gnPwd <- "admin"
-gnVersion <- "3.x"
+gnVersion <- "3.0.5"
 gnLogger <- "DEBUG"
 GN <- try(GNManager$new(gnUrl, gnUsr, gnPwd, gnVersion, gnLogger))
 
